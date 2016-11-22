b8aa60046bdb00fce2d7ff344f245bc0a0ae1fe1
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-11-21 16:51:58 -0800

    BulletGraph.js initialize

---
+++ src/BulletGraph.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import data from './data';
+
+export default class BulletGraph extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div></div>
+  }
