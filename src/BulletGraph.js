d07f1304567779a6523ca3f14ebe3f15de242c68
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-11-21 17:03:47 -0800

    add data.js file. add 'good' bar to graph

--- src/BulletGraph.js
+++ src/BulletGraph.js
@@ -4,8 +4,26 @@ import data from './data';
 export default class BulletGraph extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      ...data
+    }
   }
 
   render() {
-    <div></div>
+
+    const goodPercent = `${100* this.state.good / this.state.max}%`;
+    // console.log(this.state);
+
+    return(
+    <div>
+      <h5 id="title">BulletChart</h5>
+      <svg xmlns="http://www.w3.org/2000/svg" width="80%" height="200">
+        <g>
+          <rect width="100%" height="24px" fill="#e6e6e6" />
+          <rect width={goodPercent} height="24px" fill="#bfbfbf" />
+        </g>
+      </svg>
+    </div>)
   }
+}
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export default {
+  primaryMeasure: 73,
+  target: 80,
+  poor: 40,
+  good: 85,
+  max: 100,
+  title: "Revenue",
+  axis: "$",
+  graphWidth: "60%"
+}
